7bf24e37117e51e8d5265ebeb94d74731bc2cc96
Sanchit <sanchitbhadgal36@gmail.com>
2022-01-07 14:28:14 +0530

    Feature: clear all tasks

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,27 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import Header from './components/Header';
 import InputField from './components/InputField';
 import TodoList from './components/TodoList';
 import './App.css';
+import { AiOutlineClear } from 'react-icons/ai';
+import { TodoContext } from './context/Todo';
 
 const App: React.FC = () => {
+  const { dispatch } = useContext(TodoContext);
+
+  const handleClear = () => {
+    dispatch({ type: 'set', payload: { active: [], completed: [] } });
+  };
+
   return (
     <div className='App'>
       <span className='heading'>Taskify</span>
       <InputField />
       <Header />
       <TodoList />
+      <button className='clear-button' type='button'>
+        <AiOutlineClear onClick={handleClear} />
+      </button>
     </div>
   );
 };
